e0dcdb607c33a556ead5a60fc5ec5a6ed200fbb5
Aitor <aitorlvarez@gmail.com>
2015-11-23 21:11:20 +0100

    Repite tu nombre las veces que quieras(Errores y warnings corregidos)

---
+++ repetir_nombre.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLANCO "\x1b[37m"
+#define CYAN "\x1b[36m"
+#define NARANJA "\x1b[35m"
+#define RESET "\x1b[0m"
+
+int main(int argc, const char **argv)
+{
+    int veces,i;  //Definimos como "i" un contador y como "veces" las repeticiones que tendrán lugar
+    char nombre [10];
+
+    printf(BLANCO"¿Como te llamas? "CYAN);
+    scanf("%s",nombre);
+    printf(BLANCO"¿Cúantas veces quieres que escriba tu nombre? "CYAN);
+    scanf("%i" RESET,&veces);
+
+    i=1;    //Empieza el contador(contando la primera vez)
+
+    do{
+        printf(NARANJA "%s\n" RESET,nombre); //Escribe el nombre
+        i++;
+    }
+    while(i<=veces); //Repetirá el nombre hasta llegar al número de veces solicitadas
+
+    return EXIT_SUCCESS;
+}
+
